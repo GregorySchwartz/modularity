3f77a33374a3ffb2e9d12c0a33a6b87586c06481
Gregory W. Schwartz <gsch@pennmedicine.upenn.edu>
2018-01-11 12:07:37 -0500

    Need generic instance for Q for aeson.

--- src/Math/Modularity/Types.hs
+++ src/Math/Modularity/Types.hs
@@ -5,11 +5,13 @@ Collects the types used in modularity.
 -}
 
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Math.Modularity.Types where
 
 -- Remote
+import GHC.Generics (Generic)
 
 -- Local
 
-newtype Q = Q { unQ :: Double } deriving (Eq, Ord, Read, Show, Num)
+newtype Q = Q { unQ :: Double } deriving (Eq, Ord, Read, Show, Num, Generic)
